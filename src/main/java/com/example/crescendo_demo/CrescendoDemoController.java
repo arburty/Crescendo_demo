02447adff60226661dda2df7cfd2df24a4f5df63
arburty <austin@burt.us.com>
2021-11-09 13:55:17 -0700

    Add a logger to CrescendoDemoController.

--- src/main/java/com/example/crescendo_demo/CrescendoDemoController.java
+++ src/main/java/com/example/crescendo_demo/CrescendoDemoController.java
@@ -1,5 +1,7 @@
 package com.example.crescendo_demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,9 +9,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class CrescendoDemoController {
 
+	private static final Logger log = LoggerFactory.getLogger(CrescendoDemoController.class);
+
     @GetMapping(value = "/yelp/reviews")
-    public String allReviews(@RequestParam(value = "restaurant"
-                , defaultValue = "casa-bonita-lakewood-2") String restaurant) {
+    public String allReviews(
+            @RequestParam(value = "restaurant" , defaultValue = "casa-bonita-lakewood-2") String restaurant)
+    {
+        log.info("--CrescendoDemoController#AllReviews--");
         return restaurant;
     }
 }
