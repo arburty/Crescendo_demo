ea314c3048d4b41fd2c6af32869ef019ca95f7f1
arburty <austin@burt.us.com>
2021-11-09 13:21:37 -0700

    First Commit, immediately after unzipping project.

---
+++ src/main/java/com/example/crescendo_demo/CrescendoDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.crescendo_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrescendoDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrescendoDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/crescendo_demo/CrescendoDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crescendo_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrescendoDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
