ef7b0c9750fb563e9c0c2cf9f3f8b982fa814da3
arburty <austin@burt.us.com>
2021-11-09 15:55:52 -0700

    Finish the RestaurantReviews class by adding Reviews. Finish Endpoint and return all the data.  Create a pretty print endpoint.

--- src/main/java/com/example/crescendo_demo/CrescendoDemoController.java
+++ src/main/java/com/example/crescendo_demo/CrescendoDemoController.java
@@ -1,5 +1,7 @@
 package com.example.crescendo_demo;
 
+import java.util.ArrayList;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,14 +19,38 @@ public class CrescendoDemoController {
 	private static final Logger log = LoggerFactory.getLogger(CrescendoDemoController.class);
 
     @GetMapping(value = "/yelp/reviews", produces = "application/json")
-    public String allReviews(
+    public RestaurantReviews allReviews(
             @RequestParam(value = "restaurant" , defaultValue = "casa-bonita-lakewood-2") String restaurant)
     {
-        log.info("--CrescendoDemoController#AllReviews--");
+        log.info("--CrescendoDemoController#allReviews--");
         log.debug("restaurant:" + restaurant);
-        // Object as a placeholder for now.
         RestaurantReviews restReviews = restTemplate.getForObject("https://api.yelp.com/v3/businesses/" + restaurant + "/reviews", RestaurantReviews.class);
-        log.info("Total: " + restReviews.getTotal());
-        return restaurant;
+        return restReviews;
+    }
+
+    @GetMapping(value = "/yelp/justreviews", produces = "application/json")
+    public  ArrayList<String> justReviews(
+            @RequestParam(value = "restaurant" , defaultValue = "casa-bonita-lakewood-2") String restaurant)
+    {
+        log.info("--CrescendoDemoController#justReviews--");
+        ArrayList<String> result = new ArrayList<>();
+
+        RestaurantReviews restReviews = restTemplate.getForObject("https://api.yelp.com/v3/businesses/" + restaurant + "/reviews", RestaurantReviews.class);
+
+        ArrayList<Reviews> reviews = restReviews.getReviews();
+        for (Reviews rv : reviews) {
+            result.add(rv.getUser().getName() + " Rated " + restaurant + " a " +
+                    rv.getRating() + "/5, and said:\n" +
+                    rv.getText());
+        }
+        return result;
+    }
+
+    // simple help message.
+    @GetMapping(value = "/help")
+    public String help() {
+        return "the availble endpoints are:\n" +
+            "GET: '/yelp/reviews'\n" +
+            "   param: restaurant defaultValue='casa-bonita-lakewood-2'";
     }
 }
--- src/main/java/com/example/crescendo_demo/RestaurantReviews.java
+++ src/main/java/com/example/crescendo_demo/RestaurantReviews.java
@@ -1,16 +1,27 @@
 package com.example.crescendo_demo;
 
+import java.util.ArrayList;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class RestaurantReviews {
     private int total;
+    private ArrayList<Reviews> reviews;
 
     public int getTotal() {
         return this.total;
     }
 
+    public ArrayList<Reviews> getReviews() {
+        return this.reviews;
+    }
+
     public void setTotal(int total) {
         this.total = total;
+    }
+
+    public void setReviews(ArrayList<Reviews> reviews) {
+        this.reviews = reviews;
     }
 }
