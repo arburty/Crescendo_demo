e2b2143825c60a1126206feadf8f0d0a64276d7a
arburty <austin@burt.us.com>
2021-11-09 13:47:27 -0700

    Make endpoint more complex, adding optional RequestParam, better naming.

--- src/main/java/com/example/crescendo_demo/CrescendoDemoController.java
+++ src/main/java/com/example/crescendo_demo/CrescendoDemoController.java
@@ -1,13 +1,15 @@
 package com.example.crescendo_demo;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CrescendoDemoController {
 
-    @GetMapping(value = "/")
-    public String root() {
-        return "Hello World";
+    @GetMapping(value = "/yelp/reviews")
+    public String allReviews(@RequestParam(value = "restaurant"
+                , defaultValue = "casa-bonita-lakewood-2") String restaurant) {
+        return restaurant;
     }
 }
