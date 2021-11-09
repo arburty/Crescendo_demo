f42e738809ee7656d35f19ea52c5d1516063452d
arburty <austin@burt.us.com>
2021-11-09 15:16:48 -0700

    Finish Reviews data class.

--- src/main/java/com/example/crescendo_demo/Reviews.java
+++ src/main/java/com/example/crescendo_demo/Reviews.java
@@ -10,4 +10,53 @@ public class Reviews {
 	private int rating;
 	private String timeCreated;
 	private User user;
+
+	public String getId() {
+		return this.id;
+	}
+
+	public String getUrl() {
+		return this.url;
+	}
+
+	public String getText() {
+		return this.text;
+	}
+
+	public int getRating() {
+		return this.rating;
+	}
+
+	public String getTimeCreated() {
+		return this.timeCreated;
+	}
+
+	public User getUser() {
+		return this.user;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public void setTimeCreated(String timeCreated) {
+		this.timeCreated = timeCreated;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
 }
